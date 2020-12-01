d2dd592f3e7fb89ac2eb523f83c18f8ba98eec9b
Mikspec <mikspec@mikspec.pl>
2020-12-02 00:23:11 +0100

    Day 01 part 1

---
+++ src/Day01.ts
@@ -0,0 +1,32 @@
+
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
+import * as fs from 'fs';
+import * as readline from 'readline';
+
+class PartA {
+
+    main() {
+        let myInterface = readline.createInterface({
+            input: fs.createReadStream('data/day01.txt')
+        });
+
+        let expenses: number[] = [];
+        myInterface.on('line', (line) => {
+            expenses.push(parseInt(line));
+        });
+
+        myInterface.on('close', () => {
+            console.log('Day01 part1 =',
+                expenses.map((value1, index1) => {
+                    return expenses.filter((value2, index2) => {
+                        return ((index1 !== index2) && ((value1 + value2) === 2020))
+                    });
+                }).filter((arr: number[]) => arr.length > 0).map(arr => arr.pop()).reduce((prev, curr)=> {
+                    return prev * curr;
+                }));
+        });
+    }
+}
+
+let partA = new PartA();
+partA.main(); 
